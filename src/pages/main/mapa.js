8767b3e943317b9adf3aeeb963a50af3adc3479b
Otávio Leite R. dos Santos <otavioleitesantos@hotmail.com>
2019-11-03 17:54:22 -0300

    precisão ruim, mas funfa

--- src/pages/main/mapa.js
+++ src/pages/main/mapa.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation'
 
 import styles from './styles';
 
@@ -9,40 +10,45 @@ export default class Mapa extends Component {
     region: null
   };
 
+  componentWillUnmount() {
+    Geolocation.clearWatch(this.watchID);
+  }
+
   async componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (position) => { // sucesso
+    await Geolocation.watchPosition(
+      ({coords}) => { // sucesso
         this.setState({
           region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: 0.0140,
-            longitudeDelta: 0.0225
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+            latitudeDelta: 0.0001,
+            longitudeDelta: 0.0001
           }
         });
       },
-      (error) => this.setState({ error: error.message }), // erro
+      () => {}, // erro
       { // propriedades
+        useSignificantChanges: true,
         enableHighAccuracy: true, 
-        timeout: 2000, 
-        maximumAge: 1000 
+        timeout: 60e3, 
+        maximumAge: 10e3
       }
     );
   }
-
+  
   render() {
     const { region } = this.state;
 
     return (
       <View style={styles.container}>
         <MapView
-          region={region}
+          initialRegion={region}
           style={styles.mapView}
-          // rotateEnabled={false}
+          rotateEnabled={false}
           // scrollEnabled={false}
           // zoomEnabled={false}
-          // showsPointsOfInterest={false}
-          // showBuildings={false}
+          showsPointsOfInterest={false}
+          showBuildings={false}
           showsUserLocation={true}
           // showsMyLocationButton={true}
           // onPress={this.findPlace}
